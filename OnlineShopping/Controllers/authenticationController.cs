0581fa7546a5f387ee356c779df2e4c61e9595cb
ikmalfi3 <ikmalfi3.amran@gmail.com>
2018-04-25 17:43:39 +0800

    1st

---
+++ OnlineShopping/Controllers/authenticationController.cs
@@ -0,0 +1,45 @@
+﻿using OnlineShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace OnlineShopping.Controllers
+{
+    public class authenticationController : Controller
+    {
+        // GET: authentication
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(user user)
+        {
+            OnlineShoppingDataContext db = new OnlineShoppingDataContext();
+
+            user match = (from x in db.users
+                          where (user.email.Equals(x.email) && user.password.Equals(x.password))
+                          select x).SingleOrDefault();
+
+            if (match == null)
+            {
+                ViewBag.errMessage = match.email;
+                return View();
+            }
+            else
+            {
+                FormsAuthentication.SetAuthCookie(user.name, false);
+                return RedirectToAction("index", "Home");
+            }
+
+        }
+    }
+}
