6ef046fd3c3be4164b4a09c4beb8cc4f46e7fe42
ikmalfi3 <ikmalfi3.amran@gmail.com>
2018-05-17 16:00:19 +0800

    test

--- OnlineShopping/Controllers/customerController.cs
+++ OnlineShopping/Controllers/customerController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineShopping.Models;
+using OnlineShopping.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,9 +15,47 @@ namespace OnlineShopping.Controllers
         {
             return View();
         }
+        [AllowAnonymous][HttpGet]
         public ActionResult addCustomer()
         {
             return View();
+        }
+        [AllowAnonymous][HttpPost]
+        public ActionResult addCustomer(CustomerViewModels cust)
+        {
+            OnlineShoppingDataContext db = new OnlineShoppingDataContext();
+            int count = db.users.Count();
+            count++;
+            string id = "U";
+
+            if (count < 10) id += "00" + count.ToString();
+            else if (count < 100) id += "0" + count.ToString();
+            else if (count < 1000) id += count.ToString();
+
+            user user = new user
+            {
+                userID = id,
+                name = cust.u.name,
+                contactNo = cust.u.contactNo,
+                email = cust.u.email,
+                role = "Customer",
+                password = cust.u.password
+            };
+
+            customer customer = new customer
+            {
+                userID = id,
+                billingAddress = cust.c.billingAddress,
+                shippingAddress = cust.c.shippingAddress
+            };
+
+            
+
+            db.users.Add(user);
+            db.customers.Add(customer);
+            db.SaveChanges();
+            return View();
+
         }
         public ActionResult addCustomerConfirm()
         {
