58bed0a0008d4e46912bd1cd828d77a7dc750086
ikmalfi3 <ikmalfi3.amran@gmail.com>
2018-05-22 00:33:25 +0800

    aaaa

--- OnlineShopping/Controllers/itemController.cs
+++ OnlineShopping/Controllers/itemController.cs
@@ -1,4 +1,5 @@
 ﻿using OnlineShopping.Models;
+using OnlineShopping.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,9 @@ namespace OnlineShopping.Controllers
     [AllowAnonymous]
     public class itemController : Controller
     {
+        
+        static List<product> productList = new List<product>();
+               
         OnlineShoppingDataContext db = new OnlineShoppingDataContext();
         // GET: item
         public ActionResult Index()
@@ -28,14 +32,48 @@ namespace OnlineShopping.Controllers
             var products = db.products.ToList();
             return View(products);
         }        
+        [HttpGet]
         public ActionResult AddCart(string id)
         {
             var product = (from x in db.products
                            where x.productID == id
                            select x).SingleOrDefault();
-            return View(product);
+            
+            bool exist = false;
+            foreach(var item in productList)
+            {
+                if (item.productID.Equals(id))
+                {
+                    exist = true;
+                    item.productQuantity++;
+                }
+            }
+            if(exist == false)
+            {
+                product.productQuantity = 1;
+                productList.Add(product);
+            }
+
+            return View(productList);
         }
-        public ActionResult Payment()
+        [HttpPost]
+        public ActionResult AddCart()
+        {
+
+            return View();
+        }
+
+        public ActionResult ConfirmOrder()
+        {
+
+
+            CustomerItemViewModel vm = new CustomerItemViewModel()
+            {
+                user =
+            };
+            return View(productList);
+        }
+        public ActionResult Payment(List<product> product)
         {
             return View();
         }
