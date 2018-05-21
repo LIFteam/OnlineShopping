822d29dbb0139fef7b75a8c44e665a642ba18bcc
ikmalfi3 <ikmalfi3.amran@gmail.com>
2018-05-21 21:26:29 +0800

    test

--- OnlineShopping/Controllers/ownerController.cs
+++ OnlineShopping/Controllers/ownerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineShopping.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,41 @@ namespace OnlineShopping.Controllers
 {
     public class ownerController : Controller
     {
+        OnlineShoppingDataContext db = new OnlineShoppingDataContext();
         // GET: owner
         public ActionResult Index()
         {
             return View();
-        }     
+        }
+        public ActionResult ManageOwner()
+        {
+            var user = (from x in db.users
+                        where x.role.Equals("Owner")
+                        select x);
+
+            return View(user);
+        }
+        public ActionResult Edit(string id)
+        {
+            var user = (from x in db.users
+                        where x.userID.Equals(id)
+                        select x).SingleOrDefault();
+
+            return View(user);
+        }        
+        [HttpPost]
+        public ActionResult Edit(user userUpdate)
+        {
+            var user = (from x in db.users
+                        where x.userID.Equals(userUpdate.userID)
+                        select x).SingleOrDefault();
+
+            user.name = userUpdate.name;
+            user.contactNo = userUpdate.contactNo;
+            user.password = userUpdate.password;
+
+            db.SaveChanges();
+            return View(user);
+        }
     }
 }
